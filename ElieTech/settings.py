7e133dcd059e1cf5609173cad1ce000fe73b0c4e
ElieDev1 <148321859+ElieDev1@users.noreply.github.com>
2025-02-07 19:50:43 +0200

    Deploying to heroku

--- ElieTech/settings.py
+++ ElieTech/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1fbvd-u%sy8(mmkip20obi)ari6p02gkuq4j+n*)wp)r1lmyqn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['elietech.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ElieTech.wsgi --log-file -
